3a6876e4317e26c16508b1a3159e8f42223c4e5e
Nitin <nitinparjapat94@gail.com>
2025-07-30 17:49:52 +0530

    Resolved

--- hotel_prj/settings.py
+++ hotel_prj/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7rwndw#hwmeti5^2j-8rw!j0b5lke#pmws)a7e^399nrmglpfr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
