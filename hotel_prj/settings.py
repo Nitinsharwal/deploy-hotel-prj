6085fc2d49bab16c4f578acec7613f3a1dfa524e
Nitin <nitinparjapat94@gail.com>
2025-12-26 13:22:11 +0530

    main

--- hotel_prj/settings.py
+++ hotel_prj/settings.py
@@ -77,26 +77,26 @@ WSGI_APPLICATION = 'hotel_prj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'hotel_db',  
-#         'USER': 'root',     
-#         'PASSWORD': '', 
-#         'HOST': 'localhost',
-#         'PORT': '3306',                 
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hotel_db',  
+        'USER': 'root',     
+        'PASSWORD': '', 
+        'HOST': 'localhost',
+        'PORT': '3306',                 
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
